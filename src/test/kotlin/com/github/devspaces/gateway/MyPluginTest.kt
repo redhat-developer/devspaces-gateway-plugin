71098f664089c3055c417bde92ead3bf3666b527
Artem Zatsarynnyi <azatsary@redhat.com>
2024-02-02 16:12:39 +0100

    DevSpaces Gateway plugin (#10)
    * initial DevSpaces Gateway plugin
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    * Update src/main/resources/messages/DevSpacesBundle.properties
    
    Co-authored-by: Ilya Buziuk <ilyabuziuk@gmail.com>
    
    * Update src/main/resources/messages/DevSpacesBundle.properties
    
    Co-authored-by: Ilya Buziuk <ilyabuziuk@gmail.com>
    
    * Update src/main/resources/messages/DevSpacesBundle.properties
    
    Co-authored-by: Ilya Buziuk <ilyabuziuk@gmail.com>
    
    * Disable tests generated from a plugin template
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    
    ---------
    
    Signed-off-by: Artem Zatsarynnyi <azatsary@redhat.com>
    Co-authored-by: Ilya Buziuk <ilyabuziuk@gmail.com>

---
+++ src/test/kotlin/com/github/devspaces/gateway/MyPluginTest.kt
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2024 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+package com.github.devspaces.gateway
+
+//import com.intellij.ide.highlighter.XmlFileType
+//import com.intellij.psi.xml.XmlFile
+import com.intellij.testFramework.TestDataPath
+import com.intellij.testFramework.fixtures.BasePlatformTestCase
+import com.intellij.util.PsiErrorElementUtil
+
+@TestDataPath("\$CONTENT_ROOT/src/test/testData")
+class MyPluginTest : BasePlatformTestCase() {
+
+//    fun testXMLFile() {
+//        val psiFile = myFixture.configureByText(XmlFileType.INSTANCE, "<foo>bar</foo>")
+//        val xmlFile = assertInstanceOf(psiFile, XmlFile::class.java)
+//
+//        assertFalse(PsiErrorElementUtil.hasErrors(project, xmlFile.virtualFile))
+//
+//        assertNotNull(xmlFile.rootTag)
+//
+//        xmlFile.rootTag?.let {
+//            assertEquals("foo", it.name)
+//            assertEquals("bar", it.value.text)
+//        }
+//    }
+
+    fun testRename() {
+//        myFixture.testRename("foo.xml", "foo_after.xml", "a2")
+    }
+
+    override fun getTestDataPath() = "src/test/testData/rename"
+}
