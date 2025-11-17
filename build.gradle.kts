7582da2c292d387303b60a1d46b32882be6f785d
Victor Rubezhny <vrubezhny@redhat.com>
2025-11-17 13:54:16 +0100

    feat: Gateway: The workspaces listed in the "Connect to Dev Spaces" wizard should have their states updated automatically #23653
    Fixes: https://github.com/eclipse-che/che/issues/23653
    
    Signed-off-by: Victor Rubezhny <vrubezhny@redhat.com>
    Assisted by: OpenAI ChatGPT

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,16 @@ dependencies {
     testImplementation("io.mockk:mockk:1.14.6")
     testImplementation("io.mockk:mockk-agent-jvm:1.14.6")
 
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2")
+    // Do NOT bundle kotlinx-coroutines: IntelliJ/Gateway provides its own version.
+    // Adding another copy causes classloader conflicts, broken cancellation, and runtime errors.
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.10.2") {
+        // prevent coroutines-core from ending up inside the plugin
+        exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
+    }
+    configurations.all {
+        // prevent coroutines-core from ending up inside the plugin
+        exclude(group = "org.jetbrains.kotlinx", module = "kotlinx-coroutines-core")
+    }
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
